09b505349ed90cf401188a5c76d66792ad31e85d
Mat7400 <mmatuha74@gmail.com>
2022-04-26 19:42:13 +0300

    oop example calc

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CalcValut
+{
+    public enum valut
+    { 
+        RUR,
+        EUR,
+        USD,
+        GBP,
+        CHF,
+        YEN
+    }
+    //ООП - со,ираем полезный код в ,и,леотеку и используем его в други[ места[
+    public class calcValut
+    {
+        private double kursEvro = 80.04;
+        private double kursUsd = 71.25;
+        public double kalkulateKurs(int sum, valut Valut = valut.USD)
+        {
+            double res = 0;
+            GetKursFromInternet();
+            if (Valut == valut.USD)
+            {
+                 res = sum * kursUsd;
+            }
+            if (Valut == valut.EUR)
+            {
+                 res = sum * kursEvro;
+            }
+
+            return res;
+        }
+
+        private double GetKursFromInternet()
+        {
+            //not implemented
+            return 0;
+        }
+    }
+}
---
+++ UseLibrary/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UseLibrary
+{
+    public partial class Form1 : Form
+    {
+        CalcValut.calcValut calc = new CalcValut.calcValut();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //используем калькулятор из библиотеки написанной "дргуим программистом"
+            double res = calc.kalkulateKurs(100,CalcValut.valut.USD);
+            MessageBox.Show(res.ToString());
+        }
+    }
+}
